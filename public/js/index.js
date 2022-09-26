80059cd92199b233790a62c8123ad041ccc61ae6
mariuszgit <mariusz.zawierucha@gmail.com>
2022-09-26 21:28:49 +0000

    added printList function

--- public/js/index.js
+++ public/js/index.js
@@ -47,11 +47,39 @@ async function sendData(data) {
 async function printList() {
     const response = await fetch('/api');
     const json = await response.json();
-    
+    console.log(json);
     json.forEach(element => {
-        const list = document.querySelector('.list');
-        const list_item = document.createElement('div');
+        createListItem(element)
     });
 }
 
 printList();
+
+function createListItem(element) {
+    const list = document.querySelector('.list');
+    const list_item = document.createElement('div');
+        list_item.classList.add('list-item');
+        list_item.setAttribute('data-id', element._id);
+    const list_content = document.createElement('div');
+        list_content.classList.add('list-item__content');
+    const list_category = document.createElement('div');
+        list_category.classList.add('list-item__category');
+    const list_description = document.createElement('div');
+        list_description.classList.add('list-item__description');
+    const list_title = document.createElement('div');
+        list_title.classList.add('list-item__title');
+        list_title.textContent = element.title;
+    const list_details = document.createElement('div');
+        list_details.classList.add('list-item__details');
+        list_details.textContent = element.description;
+    const list_check = document.createElement('div');
+        list_check.classList.add('list-item__check');
+
+    
+    
+    list_description.append(list_title, list_details);
+    list_content.append(list_category, list_description);
+    list_item.append(list_content, list_check);
+    list.appendChild(list_item);
+
+}
